a68eef53fa530bd7dc7877fe5c90b00f4df88b3e
1182361276@qq.com <1182361276@qq.com>
2024-02-12 21:25:14 +0800

    粘包测试客户端

--- syncClient.cpp
+++ syncClient.cpp
@@ -1,38 +1,57 @@
 ﻿#include<iostream>
 #include<boost/asio.hpp>
+#include<thread>
+#define HEAD_LENGTH 2
 const unsigned int MAX_LENGTH = 1024;
-using namespace boost::asio;
+using namespace boost::asio::ip;
+using namespace std;
 //测试git是否存储
-int main() {
-	try {
-		// Step 1. Here we assume that the server is listening on port 3333
-		ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8080);	//这里会自动推断ip协议版本
-		// Step 2. Creating and connecting the socket.
-		io_context ioc;
-		boost::system::error_code ec;
-		ip::tcp::socket sock(ioc);
-		sock.connect(ep,ec);
-		if (ec) {
-			std::cerr << "Error: " << ec.message() << std::endl;
-			return 0;
-		}
-		// Step 3. At this point the connection is established. We can send data to the server.
-		char request[MAX_LENGTH];
-		std::cin.getline(request, MAX_LENGTH);
-		size_t request_length = strlen(request);
-		write(sock, buffer(request, request_length));
-		// Step 4. This is the blocking call. The program will wait here until data is received.
-		char reply[MAX_LENGTH];
-		size_t reply_length = read(sock, buffer(reply, request_length));
-		std::cout << "Reply is: ";
-		std::cout.write(reply, reply_length);
-		std::cout << "\n";
-		// Step 5. Shutting down the connection and the socket.
-		sock.shutdown(ip::tcp::socket::shutdown_both);
-	}
-	catch (boost::system::system_error& e) {
-		std::cerr << "Error code: " << e.code() << std::endl;
-		std::cerr << "Error message: " << e.what() << std::endl;
-	}
-	return 0;
+int main()
+{
+    try {
+        //创建上下文服务
+        boost::asio::io_context   ioc;
+        //构造endpoint
+        tcp::endpoint  remote_ep(address::from_string("127.0.0.1"), 10086);
+        tcp::socket  sock(ioc);
+        boost::system::error_code   error = boost::asio::error::host_not_found; ;
+        sock.connect(remote_ep, error);
+        if (error) {
+            cout << "connect failed, code is " << error.value() << " error msg is " << error.message();
+            return 0;
+        }
+        thread send_thread([&sock] {
+            for (;;) {
+                this_thread::sleep_for(std::chrono::milliseconds(2));
+                const char* request = "hello world!";
+                size_t request_length = strlen(request);
+                char send_data[MAX_LENGTH] = { 0 };
+                memcpy(send_data, &request_length, 2);
+                memcpy(send_data + 2, request, request_length);
+                boost::asio::write(sock, boost::asio::buffer(send_data, request_length + 2));
+            }
+            });
+        thread recv_thread([&sock] {
+            for (;;) {
+                this_thread::sleep_for(std::chrono::milliseconds(2));
+                cout << "begin to receive..." << endl;
+                char reply_head[HEAD_LENGTH];
+                size_t reply_length = boost::asio::read(sock, boost::asio::buffer(reply_head, HEAD_LENGTH));
+                short msglen = 0;
+                memcpy(&msglen, reply_head, HEAD_LENGTH);
+                char msg[MAX_LENGTH] = { 0 };
+                size_t  msg_length = boost::asio::read(sock, boost::asio::buffer(msg, msglen));
+                std::cout << "Reply is: ";
+                std::cout.write(msg, msglen) << endl;
+                std::cout << "Reply len is " << msglen;
+                std::cout << "\n";
+            }
+            });
+        send_thread.join();
+        recv_thread.join();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << endl;
+    }
+    return 0;
 }
